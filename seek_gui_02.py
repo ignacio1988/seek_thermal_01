7cf55581b0b639fb4e8c8dc162594738a9cbd4a9
ignacio1988 <ignacio.sanhueza.ruiz@gmail.com>
2017-10-04 14:58:18 -0300

    Update

--- seek_gui_02.py
+++ seek_gui_02.py
@@ -1,13 +1,12 @@
 from PyQt4 import QtGui, QtCore, uic
 from PyQt4.QtGui import *
-import scipy.misc
-import time
-import matplotlib.pyplot as plt
 from connect_seek_02 import *
 from libtiff import TIFF, TIFFimage
 import sys
 import numpy
 import os
+import datetime
+import time
 
 cam_connect = ConnectSeek()
 cam_connect.query()
@@ -16,7 +15,10 @@ form_class = uic.loadUiType("cam.ui")[0]
 i = 0
 fps_t = 0
 fps_f = 0
+seek_img16 = None
 bad_pixels = []
+hexa_pixels = []
+calibrated = False
 
 
 class MyWindowClass(QtGui.QMainWindow, form_class):
@@ -32,13 +34,17 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
                                 QtGui.QImage.Format_Indexed8)  # Declara Qimage Buffer
         self.buff_array = numpy.zeros((208, 256))
         self.additionF16 = numpy.zeros((208, 156))
+        seek_img16 = cam_connect.get_image()
+        self.hexa_pixels = self.PatterHexa_16bit01(seek_img16)
 
 
     def Adq(self):
+        global seek_img16
         seek_img16 = cam_connect.get_image()
         if seek_img16 is not None:
             if self.CorrectPatternHexa.isChecked():
-                seek_img16 = self.PatterHexa_16bit(seek_img16)
+                seek_img16 = self.CorrectHexPatt(hexa_pixels, seek_img16)
+
 
             if self.CorrectBadPixels.isChecked():
                 bad_pixels = self.FindBadPixel(seek_img16)
@@ -53,8 +59,8 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
 
 
     def show_frame(self):
-        global fps_t
-        global fps_f
+        global fps_t, fps_f
+
         now = int(time.time())
         fps_f += 1
         if fps_t == 0:
@@ -65,27 +71,24 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
             fps_f = 0
 
     def rec_image(self):
-        global i
-        rec_img16 = cam_connect.get_image()
+        global seek_img16, i
+
         if self.CorrectPatternHexa.isChecked():
-            rec_img16 = self.PatterHexa_16bit(rec_img16)
+            rec_img16 = self.CorrectHexPatt(hexa_pixels, seek_img16)
         i += 1
         if not os.path.exists('./images'):
             os.makedirs('./images')
-        name_img = './images/thermalseek_%d.tiff' % (i,)
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        name_img = './images/thermalseek_%d_%s.tiff' % (i, st)
         tiff = TIFF.open(name_img, mode='w')
-        tiff.write_image(rec_img16)
-        #scipy.misc.imsave(name_img, rec_img16)
+        tiff.write_image(seek_img16)
 
     def StartSeek(self):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.Adq)
         self.timer.start(1000. / 30)
 
-    def Starthist(self):
-        self.timer3 = QtCore.QTimer()
-        self.timer3.timeout.connect(self.histogram)
-        self.timer3.start(1000. / 30)
 
     def RecStop(self, pressed):
         if pressed:
@@ -101,24 +104,36 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
             self.timer2.stop()
 
 
-    def PatterHexa_16bit(self, img):
+    def CorrectHexPatt(self, hexap, img):
+        array = img
+        for row, col in hexap:
+            sum_near = array[row - 1, col - 1] + array[row - 1, col + 1] + \
+                       array[row + 1, col - 1] + array[row + 1, col + 1]  # Suma de los Pixeles Vecinos
+            mean_interp = sum_near / 4  # Promedio de la suma
+            array[row, col] = mean_interp
+        return array
+
+
+    def PatterHexa_16bit01(self, img):
+        global calibrated, hexa_pixels
         col = 0
         row_ini = 0
         col_ini = 10
         array = img
 
+
         for row in range(row_ini, 155, 1):
             for col in range(col_ini, 207, 15):
-                sum_near = array[row - 1, col - 1] + array[row - 1, col + 1] + \
-                           array[row + 1, col - 1] + array[row + 1, col + 1]  # Suma de los Pixeles Vecinos
-                mean_interp = sum_near / 4                                    # Promedio de la suma
-                array[row, col] = mean_interp                                 # Se asigna el valor
+                hexa_pixels.append([row, col])                               # Se asigna el valor
 
             col_ini = col + 8                                                 # Salto de 8 Pixeles para la siguiente Fila
             col_ini -= 207                                                    # Menos 207 para para el indice que empieza en la siguiente fila
             if col_ini > 207:
                 col_ini += 15
-        return array
+
+        return hexa_pixels
+
+
 
     def FindBadPixel(self, img):
         bad_pixels = []
@@ -145,13 +160,6 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
             if (mean_near - array[row, col]) > noiseReduction:
                 array[row, col] = mean_near
         return array
-
-    def histogram(self):
-        #rec_img16 = cam_connect.get_image()
-        #plt.hist(rec_img16, facecolor='green', alpha=0.75)
-        #plt.show()
-        pass
-
 
 
 app = QtGui.QApplication(sys.argv)
